cad58a95321162eda4c78328c4d3190fb68950e9
Simobini74 <17884614@studenti.unipi.it>
2021-08-24 13:38:27 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,14 @@
 import { Component, VERSION } from '@angular/core';
 
+class newPost {
+  nome: string;
+  valore: string; 
+  constructor(nome: string, valore: string) {
+    this.nome=nome;
+    this.valore=valore;
+  }
+}
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
@@ -8,7 +17,11 @@ import { Component, VERSION } from '@angular/core';
 export class AppComponent{
   title: string = 'Progetto app Postit' ;
   name: string ='Simone Bini';
-  postit = [];
+  postit: Array<newPost> =[
+    new newPost('pippo','pluto'),
+    new newPost('paperino','topolino')
+
+  ]
 
 
 
