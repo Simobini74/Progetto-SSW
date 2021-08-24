945532d29d71ae8b717f6b28f970af609112415f
Simobini74 <17884614@studenti.unipi.it>
2021-08-24 11:00:51 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,9 @@ import { Component, VERSION } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent{
+  title: string = 'Progetto app Postit' ;
+  name: string ='Simone Bini';
+
+
 }
