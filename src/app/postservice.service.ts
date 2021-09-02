6aa87a6126a4c2154a88b51f3d94a4696db6a59e
Simobini74 <17884614@studenti.unipi.it>
2021-09-02 15:47:39 +0200

    1 file modified

--- src/app/postservice.service.ts
+++ src/app/postservice.service.ts
@@ -5,6 +5,9 @@ import { HttpClient } from "@angular/common/http";
 @Injectable({providedIn: 'root'})
 
 export class PostserviceService {
+  apiKEY: string = '';
+  apiURL: string =
+    'https://eu-central-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/kvaas-giwjg/service/kvaas/incoming_webhook';
 
       
     } 
