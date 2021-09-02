2bcef0627ae639e505e09b14dd3d371b325d7d18
Simobini74 <17884614@studenti.unipi.it>
2021-09-02 15:49:13 +0200

    1 file modified

--- src/app/postservice.service.ts
+++ src/app/postservice.service.ts
@@ -8,6 +8,18 @@ export class PostserviceService {
   apiKEY: string = '';
   apiURL: string =
     'https://eu-central-1.aws.webhooks.mongodb-realm.com/api/client/v2.0/app/kvaas-giwjg/service/kvaas/incoming_webhook';
+    constructor(private service: HttpClient) {}
+    public getData() {
+      let promise = fetch(this.apiURL + '/get?key=' + this.apiKEY);
+      return promise;
+    }
+    public postData(data: string) {
+      let promise1 = fetch(
+        this.apiURL + '/post?key=' + this.apiKEY + '&msg=' + data,
+        { method: 'POST' }
+      );
+      return promise1;
+    }
 
       
     } 
