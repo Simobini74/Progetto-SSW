c33c69c5422c955a775b62b3567295cbb7455bc1
Simobini74 <17884614@studenti.unipi.it>
2021-09-02 16:02:12 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,6 +17,32 @@ export class AppComponent{
   name: string ='Simone Bini';
   selezione : newPost =new newPost();
   savedPosts: Array<newPost> = [];
+  log: boolean = false;
+  key = '';
+  constructor(private service: PostserviceService) {}
+  login(k: string) {
+    this.service.apiKEY = k;
+    this.service
+      .getData()
+      .then(response => response.json(), error => alert(error))
+      .then(data => {
+        let obj = JSON.parse(data);
+        for (let i in obj) {
+          this.savedPosts.push(obj[i]);
+        }
+        this.log = true;
+        this.key = k;
+      });
+  }
+  getKey() {
+    this.service.Key().then(key => {
+      fetch(this.service.apiURL + '/post?key=' + key + '&msg=' + {}, {
+        method: 'POST'
+      }).then(response => response.json(), error => alert(error));
+      this.key = key;
+    });
+    this.log = true;
+  }
 
 
 
