a972eb2160aa1c25df11ec45f5879f4e1203b8e7
Simobini74 <17884614@studenti.unipi.it>
2021-09-03 06:16:41 +0200

    2 files modified

--- src/app/nuovopostit/nuovopostit.component.ts
+++ src/app/nuovopostit/nuovopostit.component.ts
@@ -1,18 +1,20 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-nuovopostit',
   templateUrl: './nuovopostit.component.html',
   styleUrls: ['./nuovopostit.component.css']
 })
-export class NuovopostitComponent implements OnInit {
-  @Output() newPostEvent = new EventEmitter<string>();
-  constructor() { }
-  newPost(newName: string) {
-    this.newPostEvent.emit(newName);
+export class NuovopostitComponent  {
+  @Output() newPostEvent = new EventEmitter<Object>();
+
+  createPost(titolo: string, contenuto: string, importante: boolean) {
+    if (titolo != '') {
+      this.newPostEvent.emit({ titolo, contenuto, importante });
+    } else {
+      alert('Il titolo non può essere vuoto');
+    }
   }
 
-  ngOnInit() {
-  }
 
 }
